57faf63b17b63e1e660ee32dd9baeb211b3d3571
MiraclePazu <kenxcy96@gmail.com>
2019-05-24 22:39:07 -0700

    add several handlers

--- server/server.py
+++ server/server.py
@@ -20,6 +20,7 @@ db = mongo_client['db']
 db_user = db['user']
 db_post = db['post']
 db_answer = db['answer']
+db_max = db['max']
 
 ########################### Helper Functions ###########################
 
@@ -111,17 +112,17 @@ async def log_in(request):
     user_info = None if ('user_info' not in query) else query['user_info'][0]
 
     if user_info is None:
-        return web.Response(text='incorrect log-in request')
+        return web.Response(status=400, text='incorrect log-in request')
     
     user_info = json.loads(user_info)
     if not isinstance(user_info, dict):
-        return web.Response(text='incorrect user_info in log-in request')
+        return web.Response(status=400, text='incorrect user_info in log-in request')
 
     result = db_user.find_one({'_id': ObjectId(user_info['_id']), 
         'password': user_info['password']})
 
     if result is None:
-        return web.Response(text='Login Failed')
+        return web.Response(status=404, text='Login Failed')
 
     return web.Response(status=200, text='Login Succeeded')
 
@@ -132,26 +133,104 @@ async def get_post(request):
     post_info = None if ('post_info' not in query) else query['post_info'][0]
 
     if post_info is None:
-        return web.Response(text='incorrent post request')
+        return web.Response(status=400, text='incorrent post request')
 
     post_info = json.loads(post_info)
     if not instance(post_info, dict):
-        return web.Response(text='incorrect post_info in post request')
+        return web.Response(status=400, text='incorrect post_info in post request')
     
     summary_result = db_post.find_one({'_id': ObjectID(post_info['_id'])})
     answers_result = db_answer.find({'post_id': ObjectID(post_info['_id'])})
 
     if summary_result is None:
-        return web.Response(text='No such post')
+        return web.Response(status=404, text='No such post')
     
     summary_result['answers'] = []
 
     for answer in answers_result:
         summary_result['answers'].append(answer)
 
-    return web.Response(text=json.dumps(summary_result))
+    return web.Response(status=200, text=json.dumps(summary_result))
     
+# posting request handler
+@routes.get('/get/posting')
+async def get_posting(request):
+    query = get_request_query(request)
+    post_info = None if ('post_info' not in query) else query['post_info'][0]
 
+    if post_info is None:
+        return web.Response(status=400, text='incorrent posting request')
+
+    post_info = json.loads(post_info)
+    if not instance(post_info, dict):
+        return web.Response(status=400, text='incorrect post_info in posting request')
+    
+    post_id = db_max.find_one({'category': 'post_id'})
+
+    if post_id is None:
+        post_id = 0
+        db_max.insert_one({'category': 'post_id', '_id': 0})
+    else:
+        post_id = post_id['_id']
+        db_max.update_one({'category': 'post_id'}, {'$inc': {'_id': 1}})
+
+    post_info['_id'] = post_id
+    db_post.insert_one(post_info)
+
+    return web.Response(status=200, text="posting successfully")
+
+
+# answering request handler
+@routes.get('/get/answering')
+async def get_answering(request):
+    query = get_request_query(request)
+    answer_info = None if ('answer_info' not in query) else query['answer_info'][0]
+
+    if answer_info is None:
+        return web.Response(status=400, text='incorrent answering request')
+
+    answer_info = json.loads(answer_info)
+    if not instance(answer_info, dict):
+        return web.Response(status=400, text='incorrect answer_info in answering request')
+    
+    answer_id = db_max.find_one({'category': 'answer_id'})
+
+    if answer_id is None:
+        answer_id = 0
+        db_max.insert_one({'category': 'answer_id', '_id': 0})
+    else:
+        answer_id = answer_id['_id']
+        db_max.update_one({'category': 'answer_id'}, {'$inc': {'_id': 1}})
+
+    answer_info['_id'] = answer_id
+    db_answer.insert_one(answer_info)
+
+    return web.Response(status=200, text="answering successfully")
+
+# home request handler
+@routes.get('/get/home')
+async def get_home(request):
+    pass
+
+# profile request handler
+@routes.get('/get/profile')
+async def get_profile(request):
+    query = get_request_query(request)
+    user_info = None if ('user_info' not in query) else query['user_info'][0]
+
+    if user_info is None:
+        return web.Response(status=400, text='incorrect profile request')
+    
+    user_info = json.loads(user_info)
+    if not isinstance(user_info, dict):
+        return web.Response(status=400, text='incorrect user_info in home request')
+
+    result = db_user.find_one({'_id': ObjectId(user_info['_id']))
+
+    if result is None:
+        return web.Response(status=404, text='Cannot find the user')
+
+    return web.Response(status=200, text=json.dumps(result))
 
 @routes.get('/get/{info}')
 async def get(request):
